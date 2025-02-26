ffa3b2d1a38e4d34114a429499562316f6c2c9cb
El Toro Maestro <eltoro@atomlogik.de>
2025-02-26 15:18:06 -0500

    ollama now works

--- init/ellama-ext.el
+++ init/ellama-ext.el
@@ -1,5 +1,6 @@
 (use-package ellama ;; FIXME 
-  :init
+  :ensure t
+  :config
   ;; setup key bindings
   (setopt ellama-keymap-prefix "C-c e")
   ;; language you want ellama to translate to
@@ -10,16 +11,16 @@
 	  (make-llm-ollama
 	   ;; this model should be pulled to use it
 	   ;; value should be the same as you print in terminal during pull
-	   :chat-model "llama3.2"
+	   :chat-model "opencoder"
 	   :embedding-model "nomic-embed-text"
 	   :default-chat-non-standard-params '(("num_ctx" . 8192))))
   ;; Predefined llm providers for interactive switching.
   ;; You shouldn't add ollama providers here - it can be selected interactively
   ;; without it. It is just example.
   (setopt ellama-providers
-		    '(("zephyr" . (make-llm-ollama
-				   :chat-model "zephyr"
-				   :embedding-model "zephyr:7b-beta-q6_K"))
+		    '(("opencoder" . (make-llm-ollama
+				   :chat-model "opencoder"
+				   :embedding-model "opencoder:8b"))
 		      ("llama3.2" . (make-llm-ollama
 				    :chat-model "llama3.2"
 				    :embedding-model "mistral:7b-instruct-v0.2-q6_K"))))
