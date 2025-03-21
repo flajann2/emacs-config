c8c8b2c98b52c1ee742dc4efa118c0521487c098
El Toro Maestro <eltoro@atomlogik.de>
2025-03-21 16:43:40 -0500

    treesit added

--- init/treemacs-ext.el
+++ init/treemacs-ext.el
@@ -4,11 +4,15 @@
   :ensure t
   :config
     (treemacs)  ;; Open Treemacs
+
     ;; Ensure that treemacs uses nerd-icons or all-the-icons
+    (use-package treemacs-all-the-icons-nerd-fonts
+      :ensure t)
+    
     (if (require 'treemacs-nerd-icons nil 'noerror)
         (treemacs-load-theme "nerd-icons")
-      (if (require 'treemacs-all-the-icons nil 'noerror)
-          (treemacs-load-theme "all-the-icons")))
+      (if (require 'treemacs-all-the-icons-nerd-fonts nil 'noerror)
+          (treemacs-load-theme "all-the-icons-nerd-fonts")))
 
     (treemacs-map-icons-with-auto-mode-alist
      '("CMakeLists.txt")
@@ -16,16 +20,17 @@
 
     ;; show git diffs to the remote repo 
     (setq treemacs-git-commit-diff-mode t)
+    (treemacs-git-mode 'deferred)
     
     ;;(treemacs-follow-mode t)  ;; Enable following mode
-    ;;(treemacs-filewatch-mode t)  ;; Enable file watching mode
-    ;;(treemacs-fringe-indicator-mode 'always)  ;; Always show fringe indicators
-    ;;(treemacs-git-mode 'extended)  ;; Set Git mode to extended
-    )
+    (treemacs-filewatch-mode t)  ;; Enable file watching mode
+    (treemacs-fringe-indicator-mode 'always)  ;; Always show fringe indicators
+    (treemacs-git-mode 'extended)  ;; Set Git mode to extended
 
-;; Use package for treemacs-evil integration
-(use-package treemacs-evil
-  :ensure t
-  :after (treemacs evil))
+    ;; Use package for treemacs-evil integration
+    (use-package treemacs-evil
+      :ensure t
+      :after (treemacs evil)))
+
 
 (provide 'treemacs-ext)
---
+++ init/treesit-ext.el
@@ -0,0 +1,8 @@
+(use-package treesit-auto
+  :ensure t
+  :config
+  (global-treesit-auto-mode)
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  )
+
+(provide 'treesit-ext)
